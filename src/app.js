0333aec9f5f073bc3ee9808e1369c56ebdcf3a37
sarahasan17 <sarahasan.rnc@gmail.com>
2023-02-21 19:42:24 +0530

    hbs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
+//npm i hbs@4.0.1
+//Express documentation->Api Reference
 const express=require('express')
 const path=require('path')
 console.log(__dirname)
 console.log()
-const pathhtml=path.join(__dirname,'../public')
 const app=express()
 
+//define paths for express config
+const pathhtml=path.join(__dirname,'../public')
+//customizing views
+const viewpath=path.join(__dirname,'../templates')
+
+//set up handlebars, engines and views location
+app.set('view engine','hbs')
+app.set('views',viewpath)
+
+//set up static directory to serve
 app.use(express.static(pathhtml))
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'alhamdulillah',
+        name:'sara'
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About',
+        name:'Sara'
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:"Help",
+        help:'Allah helps',
+    })
+})
 /*app.get('',(req,res)=>{
     res.send('<h1>BISMILLAH!!</h1>')//app.com
 })
@@ -14,10 +43,10 @@ app.get('/help',(req,res)=>{
 })
 app.get('/about',(req,res)=>{
     app.use(express.static(pathhtml))
-})
+})*/
 app.get('/weather',(req,res)=>{
     app.use(express.static(pathhtml))
-})*/
+})
 app.listen(3000,()=>{
     console.log("Running on port 3000")
 })

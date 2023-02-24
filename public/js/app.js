e79db0568a459d382c76ff85fc98acd67621d0c0
sarahasan17 <sarahasan.rnc@gmail.com>
2023-02-24 10:41:39 +0530

    form

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,13 @@ console.log("Client size javascript file loaded.")
 // })
 
 //to view setting->other tools->developers tools->console
-fetch('http://localhost:3000/weather?address=!').then((response)=>{
+
+const weatherform= document.querySelector('form')
+const search=document.querySelector('input')
+const res=weatherform.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location=search.value
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
        if(data.error){
         console.log(data.error)
@@ -17,4 +23,5 @@ fetch('http://localhost:3000/weather?address=!').then((response)=>{
         console.log(data.data)
        }
     })
+})
 })
